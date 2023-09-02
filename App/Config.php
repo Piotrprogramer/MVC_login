7e595fc5edcd56c43a8d7e8c11dd95807e7c3f5e
Piotr <piotr.wasilewski.programista@gmail.com>
2023-09-02 22:00:36 +0200

    update 50% of section 9

--- App/Config.php
+++ App/Config.php
@@ -14,29 +14,49 @@ class Config
      * Database host
      * @var string
      */
-    const DB_HOST = 'localhost';
+    const DB_HOST = 'your-database-host';
 
     /**
      * Database name
      * @var string
      */
-    const DB_NAME = 'mvclogin';
+    const DB_NAME = 'your-database-name';
 
     /**
      * Database user
      * @var string
      */
-    const DB_USER = 'root';
+    const DB_USER = 'your-database-user';
 
     /**
      * Database password
      * @var string
      */
-    const DB_PASSWORD = '';
+    const DB_PASSWORD = 'your-database-password';
 
     /**
      * Show or hide error messages on screen
      * @var boolean
      */
     const SHOW_ERRORS = true;
+
+    /**
+     * Secret key for hashing
+     * @var boolean
+     */
+    const SECRET_KEY = 'your-secret-key';
+
+    /**
+     * Mailgun API key
+     *
+     * @var string
+     */
+    const MAILGUN_API_KEY = 'your-mailgun-api-key';
+
+    /**
+     * Mailgun domain
+     *
+     * @var string
+     */
+    const MAILGUN_DOMAIN = 'your-mailgun-domain';
 }
